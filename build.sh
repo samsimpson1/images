00807ee936fa5bba3e3f8114dd96679a64b3bca3
Sam Simpson <me@sams.wtf>
2022-10-29 22:36:26 +0100

    fix typo

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -eu
 PREFIX="ghcr.io/samsimpson1/"
 
 for IMAGE in snapraid; do
-  docker build . -t "${PREFIX}${IMAGE}:latest" -f "${image}.Dockerfile"
+  docker build . -t "${PREFIX}${IMAGE}:latest" -f "${IMAGE}.Dockerfile"
   docker push "${PREFIX}${IMAGE}:latest"
 done
