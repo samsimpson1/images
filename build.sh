b2d97b07e8cb7bff5d525f7493ced69627b62382
Sam Simpson <me@sams.wtf>
2022-11-07 17:35:27 +0000

    Add mastodon image

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -eu
 
 PREFIX="ghcr.io/samsimpson1/"
 
-for IMAGE in snapraid restic; do
+for IMAGE in mastodon restic snapraid; do
   docker build . -t "${PREFIX}${IMAGE}:latest" -f "${IMAGE}.Dockerfile"
   docker push "${PREFIX}${IMAGE}:latest"
 done
