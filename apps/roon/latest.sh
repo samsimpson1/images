699cf1ea4eba12443a636da70740a1d98d5724a4
Sam Simpson <sam@simpson.id>
2023-02-15 16:34:51 +0000

    Set product correctly in roon version check

--- apps/roon/latest.sh
+++ apps/roon/latest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERSION_STR=$(curl -sX GET "https://updates.roonlabs.com/update/?v=2&platform=linux&product=Roon&branch=production&branding=roon&version=1&curbranch=production" | grep machineversion | awk '{ split($0, a, "="); print a[2]; }')
+VERSION_STR=$(curl -sX GET "https://updates.roonlabs.com/update/?v=2&platform=linux&product=RoonServer&branch=production&branding=roon&version=1&curbranch=production" | grep machineversion | awk '{ split($0, a, "="); print a[2]; }')
 
 printf "${VERSION_STR}"
