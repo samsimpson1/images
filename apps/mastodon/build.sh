bb0888358ad1fb739d6098a633d4a5cf8902e6f8
Sam Simpson <sam@simpson.id>
2023-12-03 20:44:06 +0000

    Stop building mastodon images

--- apps/mastodon/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-VERSION=$(bash latest.sh)
-
-docker build . -t "ghcr.io/samsimpson1/mastodon:${VERSION}" --build-arg VERSION="${VERSION}"
-docker push "ghcr.io/samsimpson1/mastodon:${VERSION}"
--- apps/mastodon/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-VERSION=$(curl -sX GET "https://api.github.com/repos/mastodon/mastodon/releases/latest" | jq -r .tag_name)
-
-printf "${VERSION#*v}"
